c4c6091936e16a2bfede3e909fe5e77ddc06d752
김준원 <junis3@snu.ac.kr>
2020-11-23 00:17:59 +0900

    chore: :white_check_mark: Testing if the CI works right

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -13,13 +13,13 @@ function Main() {
         11/24 (화) 이후 방문해주시면 완성된 사이트를 만나실 수 있습니다! 감사합니다!
       </div>
       <div>
-        Hello! We are so pleasure that you've visited us. 
+        Hello! We are so happy that you've visited us. 
       </div>
       <div>
-        But the site will in construction for just a bit while.
+        But the site will be in a construction for just a bit while.
       </div>
       <div>
-        Please visit us after 11/24 (Tue) for a complete website. Thank you.
+        Please visit us after 11/24 (Tue) to see complete website. Thank you.
       </div>
     </>
   );
