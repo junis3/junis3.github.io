11909ab74195dde9a84feeb99014fe17db445bc1
김준원 <junis3@snu.ac.kr>
2020-11-22 23:21:31 +0900

    build: :construction: Finally ready to be in construction

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'index.scss';
+import Main from 'pages/Main';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Main.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <>
+      <div>
+        안녕하세요! 방문해주셔서 감사합니다! 
+      </div>
+      <div>
+        죄송하지만 잠깐동안 사이트를 뜯어 고치려고 합니다..!!! ㅜㅜㅜㅜ
+      </div>
+      <div>
+        11/24 (화) 이후 방문해주시면 완성된 사이트를 만나실 수 있습니다! 감사합니다!
+      </div>
+      <div>
+        Hello! We are so pleasure that you've visited us. 
+      </div>
+      <div>
+        But the site will in construction for just a bit while.
+      </div>
+      <div>
+        Please visit us after 11/24 (Tue) for a complete website. Thank you.
+      </div>
+    </>
+  );
+}
+
+export default Main;
