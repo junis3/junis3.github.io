4f7e62e66ef3bd5c6b05975903e33c9f328c7f32
김준원 <junis3@snu.ac.kr>
2021-06-04 03:26:26 +0900

    Changed profile pic. url

--- src/etc/consts.ts
+++ src/etc/consts.ts
@@ -1,4 +1,4 @@
-export const profileImageUrl = 'https://junie.s3.ap-northeast-2.amazonaws.com/profile.jpeg';
+export const profileImageUrl = 'https://junie.s3.ap-northeast-2.amazonaws.com/profile.png';
 
 export const emailFront = 'junis3';
 export const emailDomain = 'snu.ac.kr';
